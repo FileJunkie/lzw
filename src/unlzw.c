b295e7c17ecd5578a642ed5990f43e926bba66b0
Ilya "FileJunkie" Ershov <filejunkie@gmail.com>
2010-11-27 21:09:00 +0300

    Data reader function ready. Maybe.

--- src/unlzw.c
+++ src/unlzw.c
@@ -2,13 +2,15 @@
 
 #include "common.h"
 
+FILE* fin;
+
 int16_t get_code(){
 	static uint8_t tail = 0, tail_len = 0;
 	int16_t result = 0;
 	int newchar, i;
 
 	if(tail_len == 0){
-		if((newchar = getchar()) == EOF){
+		if((newchar = getc(fin)) == EOF){
 			return EOF;
 		}
 		if(word_len == 8){
@@ -17,7 +19,7 @@ int16_t get_code(){
 		else{
 			tail_len = 16 - word_len;
 			result = newchar << (8 - tail_len);
-			if((newchar = getchar()) == EOF){
+			if((newchar = getc(fin)) == EOF){
 				fprintf(stderr, "Bit alignment error\n");
 				exit(1);
 			}
@@ -39,6 +41,22 @@ int16_t get_code(){
 		for(i = 0; i < word_len - tail_len; i++){
 			result &= ~(1 << i);
 		}
+		if((newchar = getc(fin)) == EOF){
+			fprintf(stderr, "Bit alignment error\n");
+			exit(1);
+		}
+		tail_len = 8 - (word_len - tail_len);
+		tail = newchar;
+		for(i = 0; i < 16 - tail_len; i++){
+			tail &= ~(1 << (15 - i));
+		}
+
+		newchar >>= tail_len;
+		for(i = 0; i < 8 + tail_len; i++){
+			newchar &= ~(1 << (15 - i));
+		}
+		result |= newchar;
+		return result;
 	}
 
 	return result;
@@ -46,13 +64,21 @@ int16_t get_code(){
 
 int main(int argc, char** argv){
 
+	fin = fopen("testoutput", "rb");
+	if(fin == NULL){
+		fprintf(stderr, "Cannot open file\n");
+		exit(1);
+	}
+
 	dict_init();
 
 	putchar(get_code());
 	word_len++;
 	putchar(get_code());
 	putchar(get_code());
+	putchar(get_code());
 
 	dict_free();
+	fclose(fin);
 	return 0;
 }
