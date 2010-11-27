22757f1e234e63b3671e176915dfc5cb9ec92576
Ilya "FileJunkie" Ershov <filejunkie@gmail.com>
2010-11-27 14:51:57 +0300

    Main logic written

--- src/lzw.c
+++ src/lzw.c
@@ -119,13 +119,24 @@ void dict_add(int16_t c1, int16_t c2){
 
 int main(int argc, char** argv){
  	int16_t prevchar, nextchar;
+ 	int dict_pos;
 
 	dict_init();
 
  	prevchar = getchar();
  	while((nextchar = getchar()) != EOF){
-
+ 		dict_pos = dict_search(prevchar, nextchar);
+ 		if(dict_pos == -1){ // string not found in dictionary. emit last one and add new
+ 			print_code(prevchar);
+ 			dict_add(prevchar, nextchar);
+ 			prevchar = nextchar;
+ 		}
+ 		else{ //string found in dictionary
+ 			prevchar = dict_pos;
+ 		}
  	}
+ 	print_code(prevchar);
+ 	print_tail_finalize();
 
 	dict_free();
 	return 0;
